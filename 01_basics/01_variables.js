f339d711f153c7b29147463ad6c8af53c880207a
parth damania <parthdamania98@gmail.com>
2025-06-06 07:56:21 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail ="parthdamania98@gmail.com"
+var accountPassword ="Tanay5354"
+accountCity = "jaipur"
+
+accountEmail ="tanaydamania98@gmail.com"
+accountPassword ="123"
+accountCity ="daman"
+console.table([accountId, accountEmail, accountPassword, accountCity])
