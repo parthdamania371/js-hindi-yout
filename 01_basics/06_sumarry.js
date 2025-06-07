d109fed13696354805836ea78f8e995c77d083ee
parth damania <parthdamania98@gmail.com>
2025-06-07 16:22:51 +0000

    datatypes_summary

---
+++ 01_basics/06_sumarry.js
@@ -0,0 +1,22 @@
+//non-primative data types
+
+//array
+
+const heros =["ironman","spiderman","hulk"];
+
+//object
+
+let bio ={
+    name: "parth",
+    age: 20,
+    rollno: 10
+}
+console.log(bio)
+
+//funtions
+
+const MyFunctions = function(){
+    console.log("hello world");
+}
+
+console.log(MyFunctions);
