7447820d2f4f2bd7f8b408e2a476631b59f75eeb
parth damania <parthdamania98@gmail.com>
2025-06-08 09:22:39 +0000

    string and maths

---
+++ 01_basics/08_string.js
@@ -0,0 +1,12 @@
+const name ="parth"
+const repocount = 20
+//not good 
+//console.log(name + repocount + "valu" )
+
+//new thing is 
+console.log(`My Name Is ${name}And my Repocount ${repocount}`);
+
+const gameName = new String ("Gta");
+
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
---
+++ 01_basics/09_numberandmaths.js
@@ -0,0 +1,16 @@
+const score =400;
+//console.log(score)
+let balance = new Number (100);
+//console.log(balance);
+
+
+//++++++++++++++++++++++++++++++++Math++++++++++++++++++++++++++//
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random()*(max -min+1))+ min)
+
+
